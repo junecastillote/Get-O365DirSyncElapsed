bb097cde2b09d39a22bcdb924f84a04639d5ddd1
June Castillote <june.castillote@gmail.com>
2018-08-12 10:25:44 +0800

    Add files via upload

--- Get-O365DirSyncElapsed.ps1
+++ Get-O365DirSyncElapsed.ps1
@@ -10,7 +10,7 @@
 	===========================================================================
 
 	.LINK
-	https://www.lazyexchangeadmin.com/2018/08/monitor-azure-ad-lastdirsynctime-using.html
+	https://www.lazyexchangeadmin.com/2018/08/monitor-office-365-lastdirsynctime.html
 
 	.SYNOPSIS
 	Use Get-O365DirSyncElapsed.ps1 to query the last DirSync update time and send an email alert to specified recipients
